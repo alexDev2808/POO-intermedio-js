36f8e73da21a491b31de07c915e0fcd7c29cfa13
AlexDev2808 <colores.2808.8000@gmail.com>
2023-01-30 17:38:47 -0600

    Factory Pattern y RORO

--- recursividad.js
+++ recursividad.js
@@ -40,22 +40,57 @@ function deepCopy(subject){
 }
 
 
-const studentBase = {
-    name: undefined,
-    email: undefined,
-    age: undefined,
-    approvedCourses: undefined,
-    learningPaths: undefined,
-    socialMedia: {
-        twitter: undefined,
-        instagram: undefined,
-        facebook: undefined,
+// const studentBase = {
+//     name: undefined,
+//     email: undefined,
+//     age: undefined,
+//     approvedCourses: undefined,
+//     learningPaths: undefined,
+//     socialMedia: {
+//         twitter: undefined,
+//         instagram: undefined,
+//         facebook: undefined,
+//     }
+// };
+
+
+
+// Funcion para comprobar parametros obligatorios
+function requiredParam(param){
+    throw new Error(param + " es obligatorio")
+}
+
+// Aplicar RORO, recibir un objeto, retornar un objeto
+function createStudent({
+    // Se llama a la funcion solo cuando no se envia el parametro name o email
+    name = requiredParam("name"),
+    email = requiredParam("email"),
+    age,
+    twitter,
+    instagram,
+    facebook,
+    approvedCourses = [],
+    learningPaths = [],
+    // = {} indica que por defecto sea un objeto
+} = {}){
+    return {
+        name,
+        age,
+        email,
+        approvedCourses,
+        learningPaths,
+        socialMedia: {
+            twitter,
+            instagram,
+            facebook
+        },
     }
-};
+}
 
-const alexis = deepCopy(studentBase)
-
-Object.seal(alexis)
-
-// Preguntar si todas las propiedades de configurable es false
-Object.isSealed(alexis)
+const alexis = createStudent({
+    name: "alexis",
+    age: 22,
+    email: 'alexistenorio@gmail.com',
+    instagram: 'alexisg333',
+    twitter: 'alexxx34' 
+});
